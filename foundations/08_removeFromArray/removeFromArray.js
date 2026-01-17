c2c8e83340193cdd6c528e2e4f957c380f7fc1d0
Victor de Moura <76460793+VictorDMoura@users.noreply.github.com>
2026-01-17 23:04:56 +0000

    Implementação das funções repeatString, reverseString, removeFromArray, sumAll e leapYears; atualização dos testes para refletir as mudanças

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+  return array.filter(value => !valuesToRemove.includes(value));
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(initial, final) {
+
+    if (
+        typeof initial !== 'number' ||
+        typeof final !== 'number' ||
+        !Number.isInteger(initial) ||
+        !Number.isInteger(final) ||
+        initial < 0 ||
+        final < 0
+    ) {
+        return 'ERROR';
+    }
+
+    if (initial > final) {
+        [initial, final] = [final, initial];
+    }
+
+    let sum = 0;
+
+    for (let i = initial; i <= final; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
